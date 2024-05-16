7abc5e31912f352877e221b82a924605b5a175e1
Paulo Moreira <paultergust@gmail.com>
2024-05-16 12:49:46 +0100

    Refactor gradient_descent

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -2,14 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def linear_regression(x, w, b):
+    return np.dot(x, w) + b
+
+
+def quadratic_regression(x, w, b):
+    return np.dot((x*2), w) + b
+
+
 # function to calculate the cost
 def compute_cost(x, y, w, b):
 
     m + x.shape[0]
     cost = 0
-    
+
     for i in range(m):
-        f_wb = w * x[i] + b
+        f_wb = linear_regression(x[i], w, b)
         cost = cost + (f_wb - y[i]) ** 2
 
     total_cost = 1 / (2 * m) * cost
@@ -19,17 +27,17 @@ def compute_cost(x, y, w, b):
 def compute_gradient(x, y, w, b):
 
     """
-    Computes the gradient for linear regression 
+    Computes the gradient for linear regression
     Args:
-        x (ndarray (m,)): Data, m examples 
+        x (ndarray (m,)): Data, m examples
       y (ndarray (m,)): target values
-      w,b (scalar)    : model parameters  
+      w,b (scalar)    : model parameters
     Returns
       dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
-      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b
      """
 
-    m + x.shape[0]
+    m = x.shape[0]
     dj_dw = 0
     dj_db = 0
 
@@ -45,49 +53,34 @@ def compute_gradient(x, y, w, b):
 
     return dj_dw, dj_db
 
-def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function): 
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters):
     """
-    Performs gradient descent to fit w,b. Updates w,b by taking 
+    Performs gradient descent to fit w,b. Updates w,b by taking
     num_iters gradient steps with learning rate alpha
-    
+
     Args:
-      x (ndarray (m,))  : Data, m examples 
+      x (ndarray (m,))  : Data, m examples
       y (ndarray (m,))  : target values
-      w_in,b_in (scalar): initial values of model parameters  
+      w_in,b_in (scalar): initial values of model parameters
       alpha (float):     Learning rate
       num_iters (int):   number of iterations to run gradient descent
-      cost_function:     function to call to produce cost
-      gradient_function: function to call to produce gradient
-      
+
     Returns:
       w (scalar): Updated value of parameter after running gradient descent
       b (scalar): Updated value of parameter after running gradient descent
-      J_history (List): History of cost values
-      p_history (list): History of parameters [w,b] 
       """
-    
+
     # An array to store cost J and w's at each iteration primarily for graphing later
-    J_history = []
-    p_history = []
     b = b_in
     w = w_in
-    
+
     for i in range(num_iters):
         # Calculate the gradient and update the parameters using gradient_function
-        dj_dw, dj_db = gradient_function(x, y, w , b)     
+        dj_dw, dj_db = compute_gradient(x, y, w , b)
 
         # Update Parameters using equation (3) above
-        b = b - alpha * dj_db                            
-        w = w - alpha * dj_dw                            
+        b = b - alpha * dj_db
+        w = w - alpha * dj_dw
 
-        # Save cost J at each iteration
-        if i<100000:      # prevent resource exhaustion 
-            J_history.append( cost_function(x, y, w , b))
-            p_history.append([w,b])
-        # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(num_iters/10) == 0:
-            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
-                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
-                  f"w: {w: 0.3e}, b:{b: 0.5e}")
- 
-    return w, b, J_history, p_history #return w and J,w history for graphing
+
+    return w, b
