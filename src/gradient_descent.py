008705d6dd4941eacdfc9524a1f863769c0193ed
Paulo Moreira <paultergust@gmail.com>
2024-05-16 15:05:31 +0100

    Add logistic cost function

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -80,3 +80,27 @@ def gradient_descent(x, y, w_in, b_in, alpha, num_iters):
 
 
     return w, b
+
+def compute_cost_logistic(X, y, w, b):
+    """
+    Computes cost
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters
+      b (scalar)       : model parameter
+
+    Returns:
+      cost (scalar): cost
+    """
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+
+    cost = cost / m
+    return cost
